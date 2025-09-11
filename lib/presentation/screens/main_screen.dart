a34b0fed8e433e5f2109ddba4ea1c4eb78d606bc
trapped <trapped963963@gmail.com>
2025-09-11 23:31:59 +0300

    finilize the main screen

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -9,7 +9,7 @@ import 'package:anatomy_quiz_app/presentation/providers/user_progress_provider.d
 import 'package:anatomy_quiz_app/presentation/widgets/settings_icon_button.dart';
 import 'package:flutter/services.dart';
 import 'package:anatomy_quiz_app/presentation/providers/search_provider.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 
 class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
@@ -20,6 +20,25 @@ class MainScreen extends ConsumerStatefulWidget {
 
 class _MainScreenState extends ConsumerState<MainScreen> {
   
+  Future<bool> _showExitConfirmationDialog() async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('مغادرة التطبيق'),
+        content: const Text('هل أنت متأكد؟'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false), // User chooses to stay
+            child: const Text('البقاء'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true), // User chooses to exit
+            child: const Text('الخروج'),
+          ),
+        ],
+      ),
+    ) ?? false; // Return false if the dialog is dismissed
+  }
   @override
   void initState() {
     super.initState();
@@ -32,73 +51,83 @@ class _MainScreenState extends ConsumerState<MainScreen> {
     final settingsNotifier = ref.read(settingsProvider.notifier);
 
     return PopScope(
-        canPop: false,
-        onPopInvokedWithResult: (bool didPop ,dynamic result) {
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, dynamic result) async {
         if (!didPop) {
-          SystemNavigator.pop();
-       }
+          final bool shouldExit = await _showExitConfirmationDialog();
+          if (shouldExit) {
+            SystemNavigator.pop();
+          }
+        }
       },
 
       child: Scaffold(
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'مرحباً، ${userProgress.userName ?? ''}!',
-                      style: TextStyle(fontSize: 28.sp, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        SettingsIconButton(
-                          onIcon: Icons.volume_up,
-                          offIcon: Icons.volume_off,
-                          isOn: settings['sound']!,
-                          onPressed: () => settingsNotifier.toggleSound(),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // In the main Row
+                      Expanded( // Wrap in Expanded to give it a defined space
+                        child: AutoSizeText(
+                          'مرحباً، ${userProgress.userName ?? ''}!عبد الرحمن',
+                          style: TextStyle(fontSize: 28.sp, fontWeight: FontWeight.bold),
+                          maxLines: 1, // Ensure it stays on one line
+                          minFontSize: 15, // Set a minimum readable size
                         ),
-                        SettingsIconButton(
-                          onIcon: Icons.vibration,
-                          offIcon: Icons.smartphone,
-                          isOn: settings['haptics']!,
-                          onPressed: () => settingsNotifier.toggleHaptics(),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 60.h),
-                _buildMainButton(context,ref),
-                SizedBox(height: 20.h),
-                _buildSecondaryButton(
-                  context,
-                  title: 'يلا ندرس!',
-                  icon: Icons.map,
-                  onTap: () => context.push('/units'),
-                ),
-                SizedBox(height: 10.h),
-                _buildSecondaryButton(
-                  context,
-                  title: 'جاهز للاختبار؟!',
-                  icon: Icons.quiz,
-                  onTap: () => context.push('/quiz/select-content'),
-                ),
-                 SizedBox(height: 10.h),
-                _buildSecondaryButton(
-                  context,
-                  title: 'ابحث عن رسم', // Search for a diagram
-                  icon: Icons.search,
-                  onTap: () {
-                    // ## THE FIX: Reset the search state before navigating ##
-                    ref.read(searchQueryProvider.notifier).state = '';
-                    context.push('/search');
-                  },
-                ),
-              ],
+                      ),
+                      Row(
+                        children: [
+                          SettingsIconButton(
+                            onIcon: Icons.volume_up,
+                            offIcon: Icons.volume_off,
+                            isOn: settings['sound']!,
+                            onPressed: () => settingsNotifier.toggleSound(),
+                          ),
+                          SettingsIconButton(
+                            onIcon: Icons.vibration,
+                            offIcon: Icons.smartphone,
+                            isOn: settings['haptics']!,
+                            onPressed: () => settingsNotifier.toggleHaptics(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 60.h),
+                  _buildMainButton(context,ref),
+                  SizedBox(height: 20.h),
+                  _buildSecondaryButton(
+                    context,
+                    title: 'يلا ندرس!',
+                    icon: Icons.map,
+                    onTap: () => context.push('/units'),
+                  ),
+                  SizedBox(height: 10.h),
+                  _buildSecondaryButton(
+                    context,
+                    title: 'جاهز للاختبار؟!',
+                    icon: Icons.quiz,
+                    onTap: () => context.push('/quiz/select-content'),
+                  ),
+                   SizedBox(height: 10.h),
+                  _buildSecondaryButton(
+                    context,
+                    title: 'ابحث عن رسم', // Search for a diagram
+                    icon: Icons.search,
+                    onTap: () {
+                      // ## THE FIX: Reset the search state before navigating ##
+                      ref.read(searchQueryProvider.notifier).state = '';
+                      context.push('/search');
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -117,10 +146,6 @@ class _MainScreenState extends ConsumerState<MainScreen> {
       child: InkWell(
         onTap: () {
           context.push('/level/$lastActiveLevelId');
-          print(userProgress.lastActiveLevelTitle);
-          
-          print(lastActiveLevelTitle);
-
         },
         borderRadius: BorderRadius.circular(16.r),
         child: Container(
@@ -142,8 +167,13 @@ class _MainScreenState extends ConsumerState<MainScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text('متابعة التعلم', style: TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold, color: Colors.white)),
-                  Text(lastActiveLevelTitle, style: TextStyle(fontSize: 16.sp, color: Colors.white70)),
-                ],
+                  AutoSizeText(
+                    lastActiveLevelTitle,
+                    style: TextStyle(fontSize: 16.sp, color: Colors.white70),
+                    maxLines: 2,
+                    minFontSize: 12,
+                    overflow: TextOverflow.ellipsis, // Add '...' if it's still too long
+                  ),                ],
               ),
               // The icon is now a direct child of the Row
               Icon(Icons.send, color: Colors.white, size: 40.sp),
